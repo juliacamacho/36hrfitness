7c8451e1476479958b042e0f625e74b3cd336953
Nihar Annam <32647525+rahinroy@users.noreply.github.com>
2020-09-19 04:29:14 -0500

    i want to sleep

--- src_vanilla/scripts/index.js
+++ src_vanilla/scripts/index.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
     $( "#button" ).click(function() {
+        console.log("feefe")
+        $("#create").stop().fadeToggle("slow");
+        $("#join").stop().fadeToggle("slow");
+        // window.location.href = 'panels.html';
+        return false;
+    });
+
+    $( "#create" ).click(function() {
+        window.location.href = 'panels.html';
+        return false;
+    });
+
+    $( "#join" ).click(function() {
         window.location.href = 'panels.html';
         return false;
     });
