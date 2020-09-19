e35f74d6cc7172fc4acfe5243d420f58e61fc57a
Julia Camacho <49563129+juliacamacho@users.noreply.github.com>
2020-09-18 21:55:06 -0500

    add basic frontend structure

---
+++ src/components/Results/Results.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const results = (props) => {
+    return(
+        <div>
+
+        </div>
+    )
+};
+
+export default results;
---
+++ src/components/WebcamStream/WebcamStream.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const webcamStream = (props) => {
+    return(
+        <div>
+
+        </div>
+    )
+};
+
+export default webcamStream;
