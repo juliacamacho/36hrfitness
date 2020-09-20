2d0fa018edc4858abd446d7aa62efa6cd2d81a2e
Kevin Meng <kevin.meng.01@gmail.com>
2020-09-20 00:42:11 -0500

    add angles

--- src_vanilla/scripts/knob.js
+++ src_vanilla/scripts/knob.js
@@ -11,6 +11,10 @@ function createKnobs() {
         let myKnob = pureknob.createKnob(el.width() / 2, el.height() / 2);
         document.getElementById("pog_" + key).appendChild(myKnob.node());
 
+
+        // let cur = document.getElementById("pog_" + key);
+        // cur.innerHTML = cur.innerHTML + `<div><span style="font-size: 20px; font-weight: bold; font-family: 'Raleway', sans-serif" id="comment_elbow">Hi</span></div>`;
+
         knobs[key] = myKnob;
     }
 }
