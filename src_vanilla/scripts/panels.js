e550f10c81c2f47b8b4579ec978159bf4a9fa13c
Nihar Annam <32647525+rahinroy@users.noreply.github.com>
2020-09-20 00:07:24 -0500

    added more info

--- src_vanilla/scripts/panels.js
+++ src_vanilla/scripts/panels.js
@@ -3,11 +3,36 @@ $(document).ready(function(){
     // var numbers = ["&#10111;", "&#10111;", "&#10111;", "&#10111;", "&#10111;"]
     var num = 0
 
+
+    timer();
+    
     // }
 
   });
 
+function timer(){
+    var seconds = 0
+    var minutes = 0;
+    setInterval(function(){ 
+        if (seconds == 60){
+            seconds = 0;
+            minutes++;
+        }
+        // console.log(seconds);
+        if (seconds.toString().length == 1){
+            var secString = "0" + seconds.toString();
+        }
+        if (minutes.toString().length == 1){
+            var minString = "0" + minutes.toString();
+        }
 
+        var time = "- Session Length: " + minString + ":" + secString;
+
+        console.log(time);
+        $("#infoSess").text(time); 
+        seconds++;
+    }, 1000);
+}
 //only usuable up to 10
 function changeNum(number){
     // var unicodeVal = "\u278" + (number - 1).toString();
